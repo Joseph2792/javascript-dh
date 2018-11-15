ae208f1e42763937c515653cf8613661a2892cd0
Jose Alfaro <jalfaro@perceptiongrp.com>
2018-11-15 17:57:30 -0300

    agregar ultimos pasoss

--- clase01-js/js/ToDoList.js
+++ clase01-js/js/ToDoList.js
@@ -2,37 +2,48 @@
 
 
 window.onload = function(){
-    
-    do {
-        var cantTarea = window.prompt("Ingresar la cantidad de tareas que desea agregar");
-        console.log(cantTarea);  
-        if (isNaN(cantTarea) || cantTarea < 3) {
-            alert("el dato debe ser un numero o mayor a 3");
-        }         
-    } while ( isNaN(cantTarea) || cantTarea < 3);
-    var contTareas = [];
-    //var confirmar = window.confirm("estas seguro que deseas agregar " + cantTarea); 
-    for (var i = 0; i < cantTarea; i++) {
-        
-        do{
-            var tarea = window.prompt("Ingresar descripcion de la tarea");  
-            if (tarea == "") {
-                alert("la tarea no debe estar vacia");
-            }else{
-                contTareas.push(tarea);
+    do {      
+        do {
+            var cantTarea = window.prompt("Ingresar la cantidad de tareas que desea agregar");
+            console.log(cantTarea);  
+            if (isNaN(cantTarea) || cantTarea < 3) {
+                alert("el dato debe ser un numero o mayor a 3");
+            }         
+        } while ( isNaN(cantTarea) || cantTarea < 3);
+        var contTareas = [];
+        var confirmar = window.confirm("estas seguro que deseas agregar " + cantTarea); 
+        if (confirmar) {                    
+            for (var i = 1; i <= cantTarea; i++) {
+                
+                do{
+                    var tarea = window.prompt("Ingresar descripcion de la tarea " + i);  
+                    if (tarea == "") {
+                        alert("la tarea no debe estar vacia");
+                    }else{
+                        contTareas.push(tarea);
+                    }
+                }while( tarea == "")
+            }   
+            console.log(contTareas);
+            var elBody = document.querySelector('body');
+            
+            var h2 = document.createElement('h2');
+            h2.innerText = "To Do List - Cantidad de total " + cantTarea;
+            
+            var ul = document.createElement('ul');
+            
+            var p = document.createElement('p');
+            p.innerText = "Tareas completadas: " + restoTarea;
+
+            for (var n = 0; n < contTareas.length; n++) {
+                var li = document.createElement('li');
+                li.innerText = contTareas[n];
+                ul.append(li);
             }
-        }while( tarea == "")
-    }   
-    console.log(contTareas);
-    var ul = document.createElement('ul');
-    var elBody = document.querySelector('body');
-    
-    for (var n = 0; n < contTareas.length; n++) {
-        var li = document.createElement('li');
-        li.innerText = contTareas[n];
-        ul.append(li);
-    }
-    
-    elBody.append(ul);
-    
+            
+            elBody.append(h2);
+            elBody.append(ul);
+            elBody.append(p);
+        }
+    } while (confirmar === false);  
 }
